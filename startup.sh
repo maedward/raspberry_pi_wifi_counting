e0a6e3e6fdc54f0e18a4e165bff20d64b75e534e
edward <edward@motherapp.com>
2016-06-02 18:58:11 +0800

    change back the wlan on startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 
 #sudo airmon-ng check kill
 
-sudo airmon-ng stop mon0
+sudo airmon-ng stop wlan1mon
 sudo airmon-ng start wlan1
 cd /app/raspberry_pi_wifi_counting/
 sudo python /app/raspberry_pi_wifi_counting/pulldata.py $* &
-sudo airodump-ng --cswitch 0 mon0  --output-format csv --write /tmp/capture
+sudo airodump-ng --cswitch 0 wlan1mon  --output-format csv --write /tmp/capture
 ### The command below is for rc.local autostartup, since airodump-ng cannot run in background mode
 # sudo screen airodump-ng --cswitch 0  wlan1mon --band bg --write /tmp/capture  > /dev/null 2>&1  &
