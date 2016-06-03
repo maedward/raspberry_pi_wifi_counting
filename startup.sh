78f6025449321080c2139cc64db27e97f692641c
edward <edward@motherapp.com>
2016-06-04 01:56:24 +0800

    don't output the log in startup.sh

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ sudo mount -t tmpfs -o size=10M tmpfs /tmp/
 sudo airmon-ng stop wlan1mon
 sudo airmon-ng start wlan1
 cd /app/raspberry_pi_wifi_counting/
-sudo python /app/raspberry_pi_wifi_counting/pulldata.py $* &
-sudo airodump-ng --cswitch 0 wlan1mon  --output-format csv --write /tmp/capture
+sudo python /app/raspberry_pi_wifi_counting/pulldata.py > /dev/null 2>&1 &
+sudo airodump-ng --cswitch 0 wlan1mon  --output-format csv --write /tmp/capture > /dev/null 2>&1  &
 ### The command below is for rc.local autostartup, since airodump-ng cannot run in background mode
 # sudo screen airodump-ng --cswitch 0  wlan1mon --band bg --write /tmp/capture  > /dev/null 2>&1  &
