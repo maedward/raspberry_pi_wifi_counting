369e9174e13eb0ca0deae756725a92d38a9039a5
edward <edward@motherapp.com>
2016-06-30 02:37:07 +0800

    remove the watch

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ sudo airmon-ng stop wlan1mon
 sudo airmon-ng start wlan1
 cd /app/raspberry_pi_wifi_counting/
 sudo python /app/raspberry_pi_wifi_counting/pulldata.py > /dev/null 2>&1 &
-watch -n 60 sh /app/raspberry_pi_wifi_counting/check_connection.sh > /dev/null 2>&1
+#watch -n 60 sh /app/raspberry_pi_wifi_counting/check_connection.sh > /dev/null 2>&1
 sudo airodump-ng --cswitch 0 wlan1mon  --output-format csv --write /tmp/capture > /dev/null 2>&1
 ### The command below is for rc.local autostartup, since airodump-ng cannot run in background mode
 # sudo screen airodump-ng --cswitch 0  wlan1mon --band bg --write /tmp/capture  > /dev/null 2>&1  &
