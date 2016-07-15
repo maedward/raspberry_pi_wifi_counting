093c00cb2e1ef286dccee384ea92670687b46ad1
edward <edward@motherapp.com>
2016-07-15 11:31:42 +0800

    ping before check connection

--- check_connection.py
+++ check_connection.py
@@ -1,7 +1,11 @@
 import socket
 import subprocess
+from time import sleep
+from utils import ping
 
 REMOTE_SERVER = "www.google.com"
+network_is_connected = False
+
 def is_connected():
   try:
     # see if we can resolve the host name -- tells us if there is
@@ -15,9 +19,16 @@ def is_connected():
      pass
   return False
 
-if is_connected():
-    print "The connection is health"
-else:
+for times in range(3):
+    #ping network first
+    ping()
+    sleep(10)
+
+    if is_connected():
+        network_is_connected = True
+        exit(0)
+
+if not network_is_connected:
     print "Run restart script"
     cmd =r"sh /app/raspberry_pi_wifi_counting/check_connection.sh"
     subprocess.Popen(cmd, shell=True)
--- startup.sh
+++ startup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+#change tmp to ram disk for keeping read/write within ram
 sudo mount -t tmpfs -o size=10M tmpfs /tmp/
-#sudo airmon-ng check kill
 
+#stop all previous setuped wifi monitor mode
 sudo airmon-ng stop wlan1mon
+
+#start the wifi monitor mode
 sudo airmon-ng start wlan1
+
 cd /app/raspberry_pi_wifi_counting/
+
+#start passing scanned records to server
 sudo python /app/raspberry_pi_wifi_counting/pulldata.py > /dev/null 2>&1 &
-watch -n 60 python /app/raspberry_pi_wifi_counting/ping.py > /dev/null 2>&1 &
+
+#keep checking the network available
 (sleep 600; watch -n 120 python /app/raspberry_pi_wifi_counting/check_connection.py > /dev/null 2>&1) &
+
+#start the wifi scanning and write it to tmp folder
 sudo airodump-ng --cswitch 0 wlan1mon  --output-format csv --write /tmp/capture > /dev/null 2>&1
+
 ### The command below is for rc.local autostartup, since airodump-ng cannot run in background mode
 # sudo screen airodump-ng --cswitch 0  wlan1mon --band bg --write /tmp/capture  > /dev/null 2>&1  &
